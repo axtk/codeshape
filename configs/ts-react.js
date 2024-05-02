4be7554c552de6c5ea478792fda550966ec6ab41
axtk <axtkx@inbox.ru>
2024-05-02 07:15:34 +0300

    replace parserOptions.project string with array

--- configs/ts-react.js
+++ configs/ts-react.js
@@ -16,7 +16,7 @@ module.exports = {
                 ecmaFeatures: {
                     jsx: true,
                 },
-                project: './tsconfig.json',
+                project: ['./tsconfig.json'],
             },
             extends: [
                 'plugin:react/recommended',
--- configs/ts.js
+++ configs/ts.js
@@ -21,7 +21,7 @@ module.exports = {
             ],
             parser: '@typescript-eslint/parser',
             parserOptions: {
-                project: './tsconfig.json',
+                project: ['./tsconfig.json'],
             },
             plugins: ['@typescript-eslint'],
             settings: {
